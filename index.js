16248835931f9b450e32454dcb48b7fb90e53221
Kevin McKernan <kevin@mckernan.in>
2017-11-02 18:21:03 -0600

    Add CORS support

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ app.set('router', express.Router);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.get('/', (req, res) => res.send('Hello world!'));
 app.use('/candidates', candidateRoutes);
 app.use('/elections', electionRoutes);
