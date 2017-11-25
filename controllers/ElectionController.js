de883cad60602237cf1f2384abee81c19437b0d6
Kevin McKernan <kevin@mckernan.in>
2017-11-25 13:59:45 -0700

    Fix election controller

--- controllers/ElectionController.js
+++ controllers/ElectionController.js
@@ -1,7 +1,7 @@
 const Election = require('../models/election');
 const handleRequest = require('../helpers/handleRequest');
 
-exports.getAll = (req, res) => {
+exports.getAll = () => (req, res) => {
   if (req.userCap === 'unit') {
     Election.find({ unit: req.userUnit }, handleRequest(res));
   } else {
