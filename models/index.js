5e6900736c0ccfb8e3687f4fb384a624d9d0e670
Kevin McKernan <kevin@mckernan.in>
2018-01-20 13:10:57 -0700

    package updates

--- models/index.js
+++ models/index.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-const conn = mongoose.createConnection(process.env.DATABASE, {
-  useMongoClient: true,
-});
+const conn = mongoose.createConnection(process.env.DATABASE);
 
 module.exports = conn;
