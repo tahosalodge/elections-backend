72cc6c1554190774d4f2ca276ea7cf34625a18ac
Kevin McKernan <kevin@mckernan.in>
2018-01-21 16:19:54 -0700

    More unit import worki

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,6 +1,10 @@
 const Unit = require('../models/unit');
+const User = require('../models/user');
+const generatePassword = require('xkpasswd');
+const bcrypt = require('bcryptjs');
 const axios = require('axios');
 const { parseLocation } = require('parse-address');
+const Notify = require('../helpers/notify');
 
 class AdminController {
   static getElectionValue(unitFields, prefix, value) {
@@ -28,53 +32,124 @@ class AdminController {
     return chapters[chapterId];
   }
 
-  static async importUnit(oldId) {
-    try {
-      // Get data from old election site
-      const { data: { cmb2: { unit_fields: unitFields } } } = await this.getOldElection(oldId);
-      const address = parseLocation(this.getElectionValue(unitFields, 'unit', 'address_text'));
-      const { city, state, zip } = address;
-      // Parse to modify/remove anything
-      const unit = {
-        number: this.getElectionValue(unitFields, 'unit', 'number'),
-        chapter: this.getChapter(unitFields),
-        activeMembers: this.getElectionValue(unitFields, 'unit', 'attendance'),
-        address,
-        meetingLocation: {
-          address1: `${address.number} ${address.prefix} ${address.street}`,
-          address2: '',
-          city,
-          state,
-          zip,
-          notes: this.getElectionValue(unitFields, 'unit', 'location_details'),
-          oldAddress: this.getElectionValue(unitFields, 'unit', 'address_text'),
-        },
-        announce: this.getElectionValue(unitFields, 'unit', 'callout_timing'),
-        unitLeader: {
-          fname: this.getElectionValue(unitFields, 'leader', 'fname'),
-          lname: this.getElectionValue(unitFields, 'leader', 'lname'),
-          phone: this.getElectionValue(unitFields, 'leader', 'phone'),
-          email: this.getElectionValue(unitFields, 'leader', 'email'),
-          involvement: this.getElectionValue(unitFields, 'leader', 'involvement'),
-        },
-        adultRepresentative: {
-          fname: this.getElectionValue(unitFields, 'unit_adviser', 'fname'),
-          lname: this.getElectionValue(unitFields, 'unit_adviser', 'lname'),
-          phone: this.getElectionValue(unitFields, 'unit_adviser', 'phone'),
-          email: this.getElectionValue(unitFields, 'unit_adviser', 'email'),
-        },
-        youthRepresentative: {
-          fname: this.getElectionValue(unitFields, 'unit_representative', 'fname'),
-          lname: this.getElectionValue(unitFields, 'unit_representative', 'lname'),
-          phone: this.getElectionValue(unitFields, 'unit_representative', 'phone'),
-          email: this.getElectionValue(unitFields, 'unit_representative', 'email'),
-        },
-      };
-      const newUnit = await Unit.create(unit);
-      return newUnit;
-    } catch (e) {
-      return e;
+  static getAddress(unitFields) {
+    const address = {
+      address1: '',
+      city: '',
+      state: '',
+      zip: '',
+      notes: this.getElectionValue(unitFields, 'unit', 'location_details'),
+      original: this.getElectionValue(unitFields, 'unit', 'address_text'),
+    };
+    const parsedAddress = parseLocation(address.original);
+    if (parsedAddress === null) {
+      return {};
     }
+    if (parsedAddress.number) {
+      address.address1 = parsedAddress.number;
+    }
+    if (parsedAddress.prefix) {
+      address.address1 = `${address.address1} ${parsedAddress.prefix}`;
+    }
+    if (parsedAddress.street) {
+      address.address1 = `${address.address1} ${parsedAddress.street}`;
+    }
+    if (parsedAddress.type) {
+      address.address1 = `${address.address1} ${parsedAddress.type}`;
+    }
+    if (parsedAddress.city) {
+      address.city = parsedAddress.city;
+    }
+    if (parsedAddress.state) {
+      address.state = parsedAddress.state;
+    }
+    if (parsedAddress.zip) {
+      address.zip = parsedAddress.zip;
+    }
+    return address;
+  }
+
+  static getActiveMembers(unitFields) {
+    const attendance = this.getElectionValue(unitFields, 'unit', 'attendance');
+    if (Math.abs(attendance) === 'NaN') {
+      return 0;
+    }
+    return Math.abs(attendance);
+  }
+
+  static async createImportUser(unit) {
+    const password = generatePassword({ separators: '-' });
+    const hashedPassword = bcrypt.hashSync(password, 8);
+    const { chapter, unitLeader, number } = unit;
+    const { fname, lname, email } = unitLeader;
+    const createuserData = {
+      fname,
+      lname,
+      email,
+      chapter,
+      capability: 'unit',
+      password: hashedPassword,
+    };
+    const user = await User.create(createuserData);
+    new Notify(email).sendEmail(
+      `Tahosa Lodge Elections - Unit Import Notification for Troop ${number}`,
+      `Hey ${fname},<br />
+        Thank you for having a unit election in 2017. We have successfully imported your information from Troop ${number} from last year, and you may now log in and request an election for 2018.<br />
+        Login at https://elections.tahosa.co with the following credentials:<br /><br />
+
+        User: ${email}<br />
+        Password: ${password}<br /><br />
+
+        Please verify that your unit information was accurately imported from last year.<br />
+        If you are no longer involved with this unit, please reply so we can connect with the right person.<br />
+        If you have any questions or issues, please contact us at elections@tahosalodge.org.`,
+    );
+
+    return user;
+  }
+
+  static async importUnit(oldId) {
+    // Get data from old election site
+    const { data: { cmb2: { unit_fields: unitFields } } } = await this.getOldElection(oldId);
+    const address = parseLocation(this.getElectionValue(unitFields, 'unit', 'address_text'));
+    // Parse to modify/remove anything
+    const unit = {
+      number: this.getElectionValue(unitFields, 'unit', 'number'),
+      chapter: this.getChapter(unitFields),
+      activeMembers: this.getActiveMembers(unitFields),
+      address,
+      meetingLocation: this.getAddress(unitFields),
+      announce: this.getElectionValue(unitFields, 'unit', 'callout_timing'),
+      unitLeader: {
+        fname: this.getElectionValue(unitFields, 'leader', 'fname'),
+        lname: this.getElectionValue(unitFields, 'leader', 'lname'),
+        phone: this.getElectionValue(unitFields, 'leader', 'phone'),
+        email: this.getElectionValue(unitFields, 'leader', 'email'),
+        involvement: this.getElectionValue(unitFields, 'leader', 'involvement'),
+      },
+      adultRepresentative: {
+        fname: this.getElectionValue(unitFields, 'unit_adviser', 'fname'),
+        lname: this.getElectionValue(unitFields, 'unit_adviser', 'lname'),
+        phone: this.getElectionValue(unitFields, 'unit_adviser', 'phone'),
+        email: this.getElectionValue(unitFields, 'unit_adviser', 'email'),
+      },
+      youthRepresentative: {
+        fname: this.getElectionValue(unitFields, 'unit_representative', 'fname'),
+        lname: this.getElectionValue(unitFields, 'unit_representative', 'lname'),
+        phone: this.getElectionValue(unitFields, 'unit_representative', 'phone'),
+        email: this.getElectionValue(unitFields, 'unit_representative', 'email'),
+      },
+    };
+    const existingUser = await User.findOne({ email: unit.unitLeader.email });
+    const existingUnit = await Unit.findOne({ number: unit.number });
+    if (existingUser !== null || existingUnit !== null) {
+      throw new Error('User or unit already exists');
+    }
+    const newUnit = await Unit.create(unit);
+    const { _id: userId } = await this.createImportUser(unit);
+    unit.users = [userId];
+    await User.findOneAndUpdate({ _id: userId }, { unit: newUnit._id });
+    return unit;
   }
 }
 
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,14 +1,19 @@
 const router = require('express').Router();
 const bodyParser = require('body-parser');
 const controller = require('../controllers/adminController');
+const auth = require('../controllers/authController');
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
-router.post('/import-unit/:oldId', async (req, res) => {
+router.post('/import-unit/:oldId', auth.verifyToken, async (req, res) => {
   const { oldId } = req.params;
-  const unit = await controller.importUnit(oldId);
-  res.json(unit);
+  try {
+    const unit = await controller.importUnit(oldId);
+    res.json(unit);
+  } catch (e) {
+    res.status(400).json(e.message);
+  }
 });
 
 module.exports = router;
