aea3981f5a86b3ce69a86bfe294887ceb84c1aa1
Kevin McKernan <kevin@mckernan.in>
2018-03-31 00:57:04 -0500

    tooling updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: 'airbnb-base',
+  extends: ['airbnb-base', 'prettier'],
   rules: {
     'no-underscore-dangle': 0,
     'no-unused-vars': ['error', { ignoreRestSiblings: true }],
